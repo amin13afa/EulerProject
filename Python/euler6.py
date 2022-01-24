b8c695e4ab951aee959c7ff2778fcd1f2b8de31e
amin13afa <amin.ahadifar@gmail.com>
2022-01-24 17:14:25 +0330

    Some changes to euler6.py

--- Python/euler6.py
+++ Python/euler6.py
@@ -1,16 +1,8 @@
+#!/usr/bin/python3
 n = int(input())
 sum1, sum2 = 0, 0;
-
-# for i in range(1,n+1):
-#     sum1 += i 
-#     print(sum1)
-
 for i,j in enumerate(range(1,n+1)):
-    #print("i = ",i+1, "j = ", j)
     sum1 += i+1
     sum2 += j**2
- 
-
-
 print(abs(sum2 - sum1**2))
 
